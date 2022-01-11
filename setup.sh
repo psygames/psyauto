308db98f67f2de0e0163909c3e227f9c672d5c56
psy <yinlong@mail.jj.cn>
2022-01-11 18:40:40 +0800

    sfa

--- setup.sh
+++ setup.sh
@@ -1,4 +1,26 @@
 echo "[auto] Setup Begin ..."
-yum install -y wget openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel
-wget -P pkg -O python.tgz https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+echo "[auto] Install Basic ..."
+yum install -y wget gcc tar make 
+echo "[auto] Install Python Dependence ..."
+yum install -y openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel
+echo "[auto] Download Python-3.7.9.tgz ..."
+wget -O python.tgz https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+echo "[auto] Decompress Python-3.7.9.tgz ..."
+tar -zxvf python.tgz
+rm -rf python.tgz
+echo "[auto] Move Python-3.7.9 to /usr/local/Python-3.7.9 ..."
+mv Python-3.7.9 /usr/local
+cd /usr/local/Python-3.7.9
+echo "[auto] Python Install configure ..."
+./configure
+echo "[auto] Python Install make ..."
+make
+echo "[auto] Python Install make install ..."
+make install
+echo "[auto] Remove Old python link ..."
+rm -rf /usr/bin/python
+echo "[auto] Python link ..."
+ln -s /usr/local/bin/python3.7 /usr/bin/python
+python -V
+echo "[auto] Python Finished !!!"
 echo "[auto] Setup Finished !!!"
