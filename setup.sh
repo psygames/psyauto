d8ea42130ac9c73d5e635f5266ab57eeb8ce7937
psy <yinlong@mail.jj.cn>
2022-01-11 16:30:12 +0800

    push

--- setup.sh
+++ setup.sh
@@ -1,2 +1,4 @@
 echo "[auto] Setup Begin ..."
+yum install wget openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel
+wget https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
 echo "[auto] Setup Finished !!!"
