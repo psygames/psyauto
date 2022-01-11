3615249f265a3fbeeac18b4d6520ff297f2dde8d
psy <yinlong@mail.jj.cn>
2022-01-11 16:57:05 +0800

    aa

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 echo "[auto] Setup Begin ..."
 yum install -y wget openssl-devel bzip2-devel expat-devel gdbm-devel readline-devel sqlite-devel
-wget -O pkg/python.tgz https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
+wget -P pkg -O python.tgz https://www.python.org/ftp/python/3.7.9/Python-3.7.9.tgz
 echo "[auto] Setup Finished !!!"
